13c3b9784da043e1169505d85638d5e9bc73e21c
unknown <taniasultana.qa@gmail.com>
2023-11-13 11:10:34 -0500

    dynamic Xpath

---
+++ src/dynamicXPath/XPath.java
@@ -0,0 +1,31 @@
+package dynamicXPath;
+
+public class XPath {
+	
+	
+	/* A) 1) //select[@id='manage_select']
+	 *    2) //input[@id='ssp-service-zip']
+	 *    3) //span[@class='header-link']
+	 *    4) //a[@class='icon-geico']
+	 *    5)  //button[@id='geo-submit']
+	 *  
+	 *  B) 1) //a[@class='nav-a  ' and text()='Holiday Deals']
+	 *     2) //a[@class='nav-a  ' and text()='Best Sellers']
+	 *     3) //a[@class='nav-a  ' and text()='Amazon Basics']
+	 *     4) //a[@class='nav-a  ' and text()='Registry']
+	 *     5) //a[@class='nav-a  ' and text()='New Releases']
+	 *  
+	 *  C) 1) //a[text()='Report an incident']
+	 *     2) //a[text()='Track a claim']
+	 *     3) //a[text()='Report glass-only damage']
+	 *     4) //a[text()='Business Insurance Types']
+	 *     5) //a[text()='View More Insurance Types']
+	 *     
+	 *  D) 1) //a[contains(text(), 'Motor')]
+	 *     2) //a[contains(text(), 'Renters I')]
+	 *     3) //a[contains(text(), 'General Liability I')]
+	 *     4) //a[contains(text(), 'Travel I')]
+	 *     5) //a[contains(text(), 'Sec')]
+	 */
+
+}
